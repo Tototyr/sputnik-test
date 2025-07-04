892bebcad5cde4e58d971ccdd5c1e5c711e1f986
Ilia Myskov <mysilia46@gmail.com>
2025-07-04 23:40:51 +0300

    chore(config): add configs project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "page-bg": "#000000",
+        "card-bg": "#690dc5c6",
+        "card-border": "#E5E7EB",
+        "card-title": "#1E293B",
+        "card-text": "#475569",
+        "card-text-secondary": "#64748B",
+        accent: "#2563EB",
+        "accent-hover": "#1D4ED8",
+        "card-hover": "#F1F5F9",
+      },
+    },
+  },
+  plugins: [],
+};
