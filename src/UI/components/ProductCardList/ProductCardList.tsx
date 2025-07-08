8f5f28c3cb607e68f871f2c3ed22ce50936c63e3
Ilia Myskov <mysilia46@gmail.com>
2025-07-08 14:40:36 +0300

    style: fix ui

--- src/UI/components/ProductCardList/ProductCardList.tsx
+++ src/UI/components/ProductCardList/ProductCardList.tsx
@@ -8,7 +8,7 @@ export const ProductCardList: FC<ProductCardListProps> = ({ data }) => {
     <section aria-label="Список продуктов">
       <ul
         role="list"
-        className="grid grid-cols-4 gap-x-6 gap-y-15 max-xl:grid-cols-3 max-sm:grid-cols-2 max-xs:grid-cols-1 cursor-pointer max-xl:ml-6 max-xl:mr-6"
+        className="grid grid-cols-4 gap-x-6 gap-y-15 max-xl:grid-cols-3 max-sm:grid-cols-1 max-lg:grid-cols-2 max-xs:grid-cols-1 cursor-pointer max-xl:ml-6 max-xl:mr-6"
       >
         {data.map((product) => (
           <li key={product.id}>
